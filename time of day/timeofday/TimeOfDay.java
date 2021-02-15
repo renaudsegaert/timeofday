db133ef3c5fa3031fccda94c077bffea5db596d0
renau <renau@192.168.0.149>
2021-02-15 19:43:31 +0100

    enkel minute

--- time of day/timeofday/TimeOfDay.java
+++ time of day/timeofday/TimeOfDay.java
@@ -7,45 +7,33 @@ package timeofday;
 
 
 public class TimeOfDay {
-	private int uur;
-	private int minuten;
+	
+	private int minuten=0;
 	
 	
 	public int getUur() {
-		return uur;
+		return minuten/60;
 	}
 	public int getMinuten() {
-		return minuten;
+		return minuten%60;
 	}
 	
-	/**
-	 * Stelt de ondergrens van dit tijdstip-object in.
-	 * 
-	 * @post De ondergrens van dit tijdstip-object is gelijk aan de gegeven ondergrens.
-	 *     | getUur() == Uur
-	 
-	 */
-	public void setUur(int Uur) {
-		if (Uur>23)
+	
+	public void setUur(int uur) {
+		if (uur>23)
 			throw new IllegalArgumentException("PAS OP:te veel uur gegeven"); 
-		if (Uur<0)
+		if (uur<0)
 			throw new IllegalArgumentException("PAS OP:te weinig uur gegeve");
-		this.uur = Uur;
+		this.minuten = (minuten%60)+(60*uur);
 	}
-	/**
-	 * Stelt de ondergrens van dit tijdstip-object in.
-	 * 
-	 * @post De ondergrens van dit tijdstip-object is gelijk aan de gegeven ondergrens.
-	 *     | getMinuten() == Minuten
-	 
-	 */	
+	
 	public void setMinuten(int Minuten) {
 		if (Minuten>59)
 			throw new IllegalArgumentException("PAS OP :te veel minute gegeven") ;
 			
 		if (Minuten<0)
 			throw new IllegalArgumentException("PAS OP :te weinig minute gegeve");
-		this.minuten = Minuten;
+		this.minuten = (minuten/60)*60+ Minuten ;
 	}
 
 }
